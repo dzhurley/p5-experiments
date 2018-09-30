d9cd14c08b41a71c2f33665ded28f424054b28f6
Derek Hurley <dzhurley@gmail.com>
2018-09-30 16:26:48 -0700

    finish circle study

--- circles.js
+++ circles.js
@@ -1,5 +1,6 @@
-var frameFactor = 20;
 var halfX = window.innerWidth / 2, halfY = window.innerHeight / 2;
+var frameFactor = 20;
+var direction = 1, low, high;
 var corner;
 var a = halfX, r = 0;
 
@@ -9,19 +10,21 @@ function setup() {
 }
 
 function draw() {
-    if (r > corner) {
-        noLoop();
-    }
+    if (r > corner) direction = -1;
+    if (r < 0) direction = 1;
 
     fill(lerpColor(color(255), color(0), sin(frameCount / frameFactor)));
 
     change = sin(frameCount / (frameFactor * 2));
     ellipse(
-        halfX + cos(a / 10) * min([r, corner]),
-        halfY + sin(a / 10) * min([r, corner]),
+        halfX + (direction > 0 ? cos(a / 10) : sin(a / 10)) * min([r, corner]),
+        halfY + (direction > 0 ? sin(a / 10) : cos(a / 10)) * min([r, corner]),
         change * 60,
         change * 60
     );
-    a += random(abs(change) - 0.5, abs(change) + 0.5);
-    r += random(abs(change) - 0.5, abs(change) + 0.5);
+
+    low = (abs(change) - 0.5) * direction;
+    high = (abs(change) + 0.5) * direction;
+    a += random(low, high);
+    r += random(low, high);
 }
