663004b168704a4af45092839e34559bb0818ac1
Derek Hurley <dzhurley@gmail.com>
2018-12-30 19:23:44 -0800

    allow swapping

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 import p5 from 'p5';
 
-// import sketch from './circles';
-import sketch from './colors';
-// import sketch from './mouse';
+import circles from './circles';
+import colors from './colors';
+import mouse from './mouse';
+
+const sketches = { circles, colors, mouse };
+
+const radios = document.querySelector('.sketches');
+
+Object.keys(sketches).forEach((sketch, index) => {
+    const input = document.createElement('input');
+    input.type = 'radio';
+    input.name = 'sketch';
+    input.value = sketch;
+    if (index === 0) input.checked = true;
+    radios.append(input);
+});
+
+radios.addEventListener('change', evt => {
+    active.remove();
+    // eslint-disable-next-line no-new
+    active = new p5(sketches[evt.target.value], 'sketch');
+});
 
 // eslint-disable-next-line no-new
-new p5(sketch, 'sketch');
+let active = new p5(circles, 'sketch');
