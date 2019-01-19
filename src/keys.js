5d494097df92417fc86414d680f6bf9084e972f4
Derek Hurley <dzhurley@gmail.com>
2019-01-18 22:34:35 -0800

    shift means circles

--- src/keys.js
+++ src/keys.js
@@ -1,9 +1,14 @@
 export default s => {
     let offset = 0.0;
     let x, y;
-    let vary = 25;
+    let vary = 30;
 
     let lastKey, keyNoise;
+
+    const shiftKeys = [33, 34, 35, 36, 37, 38, 40, 41, 42, 43, 58, 60,
+        62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77,
+        78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 94, 95, 123,
+        124, 125, 126];
 
     s.setup = () => {
         s.createCanvas(s.windowWidth, s.windowHeight);
@@ -26,12 +31,21 @@ export default s => {
         );
 
         keyNoise = 200 * s.noise(lastKey);
-        s.quad(
-            x + s.random(vary - keyNoise, vary + keyNoise), y + s.random(vary - keyNoise, vary + keyNoise),
-            x + s.random(vary - keyNoise, vary + keyNoise), y - s.random(vary - keyNoise, vary + keyNoise),
-            x - s.random(vary - keyNoise, vary + keyNoise), y - s.random(vary - keyNoise, vary + keyNoise),
-            x - s.random(vary - keyNoise, vary + keyNoise), y + s.random(vary - keyNoise, vary + keyNoise),
-        );
+        if (shiftKeys.indexOf(lastKey) < 0) {
+            s.quad(
+                x + s.random(vary - keyNoise, vary + keyNoise), y + s.random(vary - keyNoise, vary + keyNoise),
+                x + s.random(vary - keyNoise, vary + keyNoise), y - s.random(vary - keyNoise, vary + keyNoise),
+                x - s.random(vary - keyNoise, vary + keyNoise), y - s.random(vary - keyNoise, vary + keyNoise),
+                x - s.random(vary - keyNoise, vary + keyNoise), y + s.random(vary - keyNoise, vary + keyNoise),
+            );
+        } else {
+            s.ellipse(
+                x,
+                y,
+                s.random(vary - keyNoise, vary + keyNoise),
+                s.random(vary - keyNoise, vary + keyNoise),
+            );
+        }
 
         offset += 0.05;
         x = s.noise(offset) * s.width;
